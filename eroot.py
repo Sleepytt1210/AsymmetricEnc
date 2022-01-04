77f68134609eabf431d229a9dff568e5d5f7dbe2
Dylon Wong <b8065261@newcastle.ac.uk>
2022-01-04 15:04:11 +0000

    Removed unused variables.

--- eroot.py
+++ eroot.py
@@ -1,17 +1,13 @@
 from math import gcd
-from utils import modinv2
+from utils import modinv
 from optparse import OptionParser
 
-p = 2111
-q = 2113
-N = 4460543
+p = p
 e = 23
 
 parser = OptionParser()
 parser.add_option("-p", "--pprime", dest="p", action="store",
                   help="Prime number p", type="int")
-parser.add_option("-q", "--qprime", action="store", dest="q",
-                  help="Prime number q", type="int")
 parser.add_option("-e", "--exponent", action="store", dest="e",
                   help="Public key/exponent e", type="int")
 parser.add_option("-m", "--message", action="store", dest="m",
@@ -20,11 +16,8 @@ parser.add_option("-m", "--message", action="store", dest="m",
 
 if options.p:
 	p = options.p
-if options.q:
-	q = options.q
 if options.e:
 	e = options.e
-N = p * q
 m = 123 if not options.m else options.m
 
 
@@ -35,7 +28,7 @@ if denom != 1:
 	sys.stderr.write("gcd of e and p-1 is not equal to 1!\n")
 	sys.exit(0)
 
-d = modinv2(e, p-1)
+d = modinv(e, p-1)
 print("Mult inverse=", d)
 
 c = pow(m, e, p)
